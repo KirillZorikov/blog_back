43bb87a8f36a4c65991f7fa3bb59c8cee20b5f96
kirill <k.zorikov@yandex.ru>
2021-04-24 13:54:54 +0500

    add search option

--- api/api_post/views.py
+++ api/api_post/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets, mixins, filters
 from rest_framework.decorators import action
+from rest_framework.filters import SearchFilter
 from rest_framework.permissions import (IsAuthenticated,
                                         IsAuthenticatedOrReadOnly, AllowAny)
 
@@ -51,9 +52,10 @@ class CommentViewSet(viewsets.ModelViewSet,
 
 class PostViewSet(viewsets.ModelViewSet,
                   LikeDislikeMixins):
-    filter_backends = (DjangoFilterBackend, PostCustomOrdering)
+    filter_backends = (DjangoFilterBackend, PostCustomOrdering, SearchFilter)
     filter_class = PostFilter
     permission_classes = (IsOwnerOrReadOnly, IsAuthenticatedOrReadOnly)
+    search_fields = ('text', 'author__username')
 
     @action(
         detail=False,
